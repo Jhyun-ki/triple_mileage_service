3dec19babf14bce0012948ed1683c4763815da02
HyunKi Jung <hjk2334@naver.com>
2022-06-28 18:56:23 +0900

    jpa test commit

---
+++ mileageService/src/main/java/com/tripleAPI/mileageService/point/controller/PointController.java
@@ -0,0 +1,8 @@
+package com.tripleAPI.mileageService.point.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PointController {
+}
---
+++ mileageService/src/test/java/com/tripleAPI/mileageService/TripleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripleAPI.mileageService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
