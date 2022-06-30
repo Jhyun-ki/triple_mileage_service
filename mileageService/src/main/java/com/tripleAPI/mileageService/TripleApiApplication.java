20ae30ec30574a4ab3c1eab01f0dc30fec093e02
HyunKi Jung <hjk2334@naver.com>
2022-07-01 00:00:44 +0900

    gradle 커밋

---
+++ mileageService/src/main/java/com/tripleAPI/mileageService/TripleApiApplication.java
@@ -0,0 +1,13 @@
+package com.tripleAPI.mileageService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleApiApplication.class, args);
+	}
+
+}
